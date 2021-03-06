a40f4a2669855bf23cb5d26b896663985f49a1a5
TimeChaser <1872101334@qq.com>
2021-03-07 03:19:21 +0800

    两数之和

--- src/top/irvingsoft/leetcode/code1/Solution.java
+++ src/top/irvingsoft/leetcode/code1/Solution.java
@@ -1,5 +1,7 @@
 package top.irvingsoft.leetcode.code1;
 
+import java.util.HashMap;
+
 /**
  * @description: 两数之和
  * @author: TimeChaser
@@ -8,6 +10,25 @@ package top.irvingsoft.leetcode.code1;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
 
-        return null;
+        /*for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return new int[0];*/
+
+        HashMap<Integer, Integer> hashMap = new HashMap<>(nums.length - 1);
+        hashMap.put(nums[0], 0);
+        for (int i = 1; i < nums.length; i++) {
+            if (hashMap.containsKey(target - nums[i])) {
+                return new int[]{hashMap.get(target - nums[i]), i};
+            }
+            hashMap.put(nums[i], i);
+        }
+
+        return new int[0];
     }
 }
