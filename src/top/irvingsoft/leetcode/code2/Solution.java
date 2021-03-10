874a86886fa1d1f0954a3b365fff413b95b35158
TimeChaser <1872101334@qq.com>
2021-03-10 16:10:55 +0800

    两数相加

--- src/top/irvingsoft/leetcode/code2/Solution.java
+++ src/top/irvingsoft/leetcode/code2/Solution.java
@@ -8,6 +8,29 @@ package top.irvingsoft.leetcode.code2;
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
 
-        return null;
+        ListNode head = null, tail = null;
+        int remainder = 0;
+
+        while (l1 != null || l2 != null) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + remainder;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            remainder = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (remainder > 0) {
+            tail.next = new ListNode(remainder % 10);
+        }
+        return head;
     }
 }
