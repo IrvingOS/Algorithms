ca1b8567e594e804dbaf9b4ae044fe7ebcc15ff8
TimeChaser <1872101334@qq.com>
2021-03-12 18:21:07 +0800

    最长回文子串

---
+++ src/top/irvingsoft/leetcode/code5/Solution.java
@@ -0,0 +1,43 @@
+package top.irvingsoft.leetcode.code5;
+
+/**
+ * @description: 最长回文子串
+ * @author: TimeChaser
+ * @date: 2021/3/12 16:36
+ */
+public class Solution {
+
+    /**
+     * @description: 暴力枚举法
+     * @author: TimeChaser
+     * @date: 2021/3/12 18:19
+     */
+    public static String longestPalindromeViolent(String s) {
+
+        return null;
+    }
+
+    /**
+     * @description: 中心扩展法
+     * @author: TimeChaser
+     * @date: 2021/3/12 18:19
+     */
+    public static String longestPalindromeViolentCenterExtension(String s) {
+        
+        return null;
+    }
+
+    /**
+     * @description: 动态规划法
+     * @author: TimeChaser
+     * @date: 2021/3/12 18:20
+     */
+    public static String longestPalindromeViolentDynamicProgramming(String s) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
