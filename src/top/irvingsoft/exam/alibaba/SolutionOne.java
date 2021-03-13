d532a769c7ff8c1e86ddcdc41f446cd18bf1c14e
TimeChaser <1872101334@qq.com>
2021-03-13 12:25:22 +0800

    阿里巴巴2021实习生招聘在线笔试

---
+++ src/top/irvingsoft/exam/alibaba/SolutionOne.java
@@ -0,0 +1,66 @@
+package top.irvingsoft.exam.alibaba;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * @description: 阿里巴巴笔试第一题
+ * @author: TimeChaser
+ * @date: 2021/3/12 18:59
+ */
+public class SolutionOne {
+
+    private static HashMap<Integer, HashSet<Integer>> relationHashMap;
+
+    public static void main(String[] args) {
+
+        int n, m, q;
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        m = in.nextInt();
+        relationHashMap = new HashMap<>(n);
+        for (int i = 0; i < m; i++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            if (relationHashMap.containsKey(a)) {
+                HashSet<Integer> bHashSet = relationHashMap.get(a);
+                bHashSet.add(b);
+            } else {
+                HashSet<Integer> bHashSet = new HashSet<>();
+                bHashSet.add(b);
+                relationHashMap.put(a, bHashSet);
+            }
+        }
+        q = in.nextInt();
+        for (int i = 0; i < q; i++) {
+            int count = 0;
+            int x = in.nextInt();
+            int y = in.nextInt();
+            if (relationHashMap.containsKey(x)) {
+                System.out.println(countRelation(count, y, relationHashMap.get(x)));
+            } else {
+                System.out.println("-1");
+            }
+        }
+    }
+
+    private static int countRelation(Integer count, Integer y, HashSet<Integer> yHashSet) {
+        count++;
+        if (yHashSet == null) {
+            count = -1;
+        } else if (!yHashSet.contains(y)) {
+            boolean flag = true;
+            for (Integer integer : yHashSet) {
+                if (relationHashMap.containsKey(integer)) {
+                    flag = false;
+                    count = countRelation(count, y, relationHashMap.get(integer));
+                }
+            }
+            if (flag) {
+                count = -1;
+            }
+        }
+        return count;
+    }
+}
---
+++ src/top/irvingsoft/exam/alibaba/SolutionTwo.java
@@ -0,0 +1,13 @@
+package top.irvingsoft.exam.alibaba;
+
+/**
+ * @description: 阿里巴巴笔试第二题
+ * @author: TimeChaser
+ * @date: 2021/3/12 18:59
+ */
+public class SolutionTwo {
+
+    public static void main(String[] args) {
+
+    }
+}
