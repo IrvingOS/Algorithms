66ae9d063c1467e2bd23c70b0a01b8a939778a6d
TimeChaser <1872101334@qq.com>
2021-03-17 21:42:17 +0800

    中心扩展法

--- src/top/irvingsoft/leetcode/code5/Solution.java
+++ src/top/irvingsoft/leetcode/code5/Solution.java
@@ -9,23 +9,43 @@ public class Solution {
 
     /**
      * @description: 暴力枚举法
+     *
+     * j - i + 1 > maxLength && isPalindromeViolent(i, j, sArray)
+     * 这一个短路判断是在暴力枚举法中节省时间的重要手段
+     *
      * @author: TimeChaser
      * @date: 2021/3/12 18:19
      */
     public static String longestPalindromeViolent(String s) {
 
+        if (s.length() <= 1) {
+            return s;
+        }
+
         int length = s.length();
+        int index = 0, maxLength = 1;
+        char[] sArray = s.toCharArray();
         for (int i = 0; i < length - 1; i++) {
             for (int j = i + 1; j < length; j++) {
-
+                if (j - i + 1 > maxLength && isPalindromeViolent(i, j, sArray)) {
+                    maxLength = j - i + 1;
+                    index = i;
+                }
             }
         }
-        return null;
+        return s.substring(index, maxLength + index);
     }
 
-    public static boolean isPalindrome(String s) {
+    public static boolean isPalindromeViolent(Integer indexBegin, Integer indexEnd, char[] sArray) {
 
-        return false;
+        while (indexBegin < indexEnd) {
+            if (sArray[indexBegin] != sArray[indexEnd]) {
+                return false;
+            }
+            indexBegin++;
+            indexEnd--;
+        }
+        return true;
     }
 
     /**
@@ -34,8 +54,36 @@ public class Solution {
      * @date: 2021/3/12 18:19
      */
     public static String longestPalindromeViolentCenterExtension(String s) {
-        
-        return null;
+
+        if (s.length() <= 1) {
+            return s;
+        }
+
+        int length = s.length();
+        int index = 0, maxLength = 1;
+        char[] sArray = s.toCharArray();
+        for (int i = 0; i < length - 1; i++) {
+            int oddLength = countMaxLengthByCenterExtension(i, i, sArray);
+            int evenLength = countMaxLengthByCenterExtension(i, i + 1, sArray);
+            int currentMaxLength = Math.max(oddLength, evenLength);
+            if (currentMaxLength > maxLength) {
+                maxLength = currentMaxLength;
+                index = i - (maxLength - 1) / 2;
+            }
+        }
+        return s.substring(index, maxLength + index);
+    }
+
+    public static int countMaxLengthByCenterExtension(Integer indexCenterLeft, Integer indexCenterRight, char[] sArray) {
+
+        while (indexCenterLeft >= 0 && indexCenterRight < sArray.length) {
+            if (sArray[indexCenterLeft] != sArray[indexCenterRight]) {
+                break;
+            }
+            indexCenterLeft--;
+            indexCenterRight++;
+        }
+        return indexCenterRight - indexCenterLeft - 1;
     }
 
     /**
@@ -49,6 +97,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
+        String s = "cbbd";
+        String palindromeViolent = longestPalindromeViolentCenterExtension(s);
+        System.out.println(palindromeViolent);
     }
 }
