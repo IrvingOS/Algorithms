618a34b9abc401d56f6c33a99829953545e7157b
TimeChaser <1872101334@qq.com>
2021-04-13 17:32:27 +0800

    反转链表

---
+++ src/top/irvingsoft/leetcode/code206/Solution.java
@@ -0,0 +1,24 @@
+package top.irvingsoft.leetcode.code206;
+
+/**
+ * @description: 反转链表
+ * @author: TimeChaser
+ * @date: 2021/4/12 11:45
+ */
+public class Solution {
+
+    public static ListNode reverseList(ListNode head) {
+
+        if (head == null) {
+            return null;
+        }
+
+        ListNode fast = head, slow = head;
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
