51da565d074c78e5c292e10e55d26d2c204c77b5
TimeChaser <1872101334@qq.com>
2021-04-19 20:43:14 +0800

    合并 K 个升序链表

--- src/top/irvingsoft/leetcode/code2/Solution.java
+++ src/top/irvingsoft/leetcode/code2/Solution.java
@@ -33,4 +33,29 @@ public class Solution {
         }
         return head;
     }
+
+    public ListNode addTwoNumbersSecond(ListNode l1, ListNode l2) {
+
+        ListNode head = new ListNode();
+        ListNode current = head;
+
+        int remainder = 0;
+        while (l1 != null || l2 != null) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + remainder;
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            remainder = sum / 10;
+        }
+        if (remainder > 0) {
+            current.next = new ListNode(remainder % 10);
+        }
+        return head.next;
+    }
 }
--- src/top/irvingsoft/leetcode/code23/Solution.java
+++ src/top/irvingsoft/leetcode/code23/Solution.java
@@ -1,13 +1,56 @@
 package top.irvingsoft.leetcode.code23;
 
+/**
+ * @description: 合并 K 个升序链表
+ *
+ * 分治法
+ *
+ * @author: TimeChaser
+ * @date: 2021/4/19 19:54
+ */
+
 public class Solution {
 
     public static ListNode mergeKLists(ListNode[] lists) {
 
-        return null;
+        if (lists.length == 0) {
+            return null;
+        }
+        return mergeList(lists, 0, lists.length - 1);
+    }
+
+    public static ListNode mergeList(ListNode[] lists, int start, int end) {
+        if (start == end) {
+            return lists[start];
+        }
+        int middle = (end - start) / 2 + start;
+        ListNode left = mergeList(lists, start, middle);
+        ListNode right = mergeList(lists, middle + 1, end);
+        return merge(left, right);
+    }
+
+    public static ListNode merge(ListNode left, ListNode right) {
+
+        ListNode head = new ListNode();
+        ListNode current = head;
+
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                current.next = left;
+                left = left.next;
+            } else {
+                current.next = right;
+                right = right.next;
+            }
+            current = current.next;
+        }
+        current.next = left == null ? right : left;
+
+        return head.next;
     }
 
     public static void main(String[] args) {
+
 
     }
 }
