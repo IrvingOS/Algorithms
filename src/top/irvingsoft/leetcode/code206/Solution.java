e6da43058218b9467e1d8a00779f7e087023e940
TimeChaser <1872101334@qq.com>
2021-04-19 18:44:08 +0800

    反转链表

--- src/top/irvingsoft/leetcode/code206/Solution.java
+++ src/top/irvingsoft/leetcode/code206/Solution.java
@@ -2,6 +2,9 @@ package top.irvingsoft.leetcode.code206;
 
 /**
  * @description: 反转链表
+ *
+ * 依次遍历链表，每次遍历到的节点会成为当前的已反转的部分的头节点，每次遍历到的下一个节点会成为当前的未反转的部分的头节点
+ *
  * @author: TimeChaser
  * @date: 2021/4/12 11:45
  */
@@ -13,9 +16,15 @@ public class Solution {
             return null;
         }
 
-        ListNode fast = head, slow = head;
+        ListNode prev = null, curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
 
-        return slow;
+        return prev;
     }
 
     public static void main(String[] args) {
---
+++ src/top/irvingsoft/leetcode/code23/Solution.java
@@ -0,0 +1,13 @@
+package top.irvingsoft.leetcode.code23;
+
+public class Solution {
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
