95ccdc20878c4c8daad90f82a60aa0994a42a8a2
TimeChaser <1872101334@qq.com>
2021-04-20 13:43:36 +0800

    合并 K 个升序链表->优先队列

--- src/top/irvingsoft/leetcode/code23/Solution.java
+++ src/top/irvingsoft/leetcode/code23/Solution.java
@@ -1,11 +1,15 @@
 package top.irvingsoft.leetcode.code23;
 
+import java.util.PriorityQueue;
+
 /**
  * @description: 合并 K 个升序链表
  *
  * 1. 顺序合并
  * 2. 分治法
  * 3. 使用优先队列合并
+ *      优先队列：
+ *
  *
  * @author: TimeChaser
  * @date: 2021/4/19 19:54
@@ -60,6 +64,42 @@ public class Solution {
         }
         current.next = left == null ? right : left;
 
+        return head.next;
+    }
+
+    static class Status implements Comparable<Status> {
+        int val;
+        ListNode ptr;
+
+        Status(int val, ListNode ptr) {
+            this.val = val;
+            this.ptr = ptr;
+        }
+
+        @Override
+        public int compareTo(Status status2) {
+            return this.val - status2.val;
+        }
+    }
+
+    static PriorityQueue<Status> queue = new PriorityQueue<>();
+
+    public static ListNode mergeKListsPriority(ListNode[] lists) {
+        for (ListNode node: lists) {
+            if (node != null) {
+                queue.offer(new Status(node.val, node));
+            }
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!queue.isEmpty()) {
+            Status f = queue.poll();
+            tail.next = f.ptr;
+            tail = tail.next;
+            if (f.ptr.next != null) {
+                queue.offer(new Status(f.ptr.next.val, f.ptr.next));
+            }
+        }
         return head.next;
     }
 
