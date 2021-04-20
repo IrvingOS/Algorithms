4865faf7bd44347b67a05408f2bb984df94b66fb
TimeChaser <1872101334@qq.com>
2021-04-20 18:03:55 +0800

    环形链表

--- src/top/irvingsoft/leetcode/code141/ListNode.java
+++ src/top/irvingsoft/leetcode/code141/ListNode.java
@@ -20,4 +20,6 @@ public class ListNode {
         this.val = val;
         this.next = next;
     }
+
+
 }
--- src/top/irvingsoft/leetcode/code141/Solution.java
+++ src/top/irvingsoft/leetcode/code141/Solution.java
@@ -1,14 +1,44 @@
 package top.irvingsoft.leetcode.code141;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * @description: 环形链表
+ *
+ * 1、哈希表
+ * 2、快慢指针
+ *
  * @author: TimeChaser
  * @date: 2021/4/20 12:31
  */
 public class Solution {
 
-    public static boolean hasCycle(ListNode head) {
+    public static boolean hasCycleHash(ListNode head) {
 
+        Set<ListNode> listNodeHashSet = new HashSet<>();
+        while (head != null) {
+            if (!listNodeHashSet.add(head)) {
+                return true;
+            }
+            head = head.next;
+        }
+        return false;
+    }
+
+    public static boolean hasCycleFastAndSlow(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+        ListNode slow = head, fast = head.next;
+        while (slow != fast) {
+            if (fast == null || fast.next == null) {
+                return false;
+            }
+            slow = slow.next;
+            fast = fast.next.next;
+        }
         return true;
     }
 }
