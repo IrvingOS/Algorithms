7420b9658f1672b59be75c22e7af16930f0f0dde
TimeChaser <1872101334@qq.com>
2021-05-06 10:26:40 +0800

    自实现 Map

---
+++ src/top/irvingsoft/test/containers/MapEntry.java
@@ -0,0 +1,60 @@
+package top.irvingsoft.test.containers;
+
+import java.util.Map;
+
+/**
+ * @description:
+ * @author: TimeChaser
+ * @date: 2021/5/6 10:20
+ */
+public class MapEntry<K, V> implements Map.Entry<K, V> {
+    private final K key;
+    private V value;
+
+    public MapEntry(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    @Override
+    public K getKey() {
+        return key;
+    }
+
+    @Override
+    public V getValue() {
+        return value;
+    }
+
+    @Override
+    public V setValue(V value) {
+        return this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof MapEntry)) {
+            return false;
+        }
+        MapEntry me = (MapEntry) o;
+        return (key == null ?
+                me.getKey() == null : key.equals(me.getKey()))
+                &&
+                (value == null ?
+                me.getValue() == null : value.equals(me.getValue()));
+    }
+
+    @Override
+    public int hashCode() {
+        return (key == null ? 0 : key.hashCode()) ^
+                (value == null ? 0 : value.hashCode());
+    }
+
+    @Override
+    public String toString() {
+        return "MapEntry{" +
+                "key=" + key +
+                ", value=" + value +
+                '}';
+    }
+}
---
+++ src/top/irvingsoft/test/containers/SlowMap.java
@@ -0,0 +1,55 @@
+package top.irvingsoft.test.containers;
+
+import java.util.*;
+
+/**
+ * @description:
+ * @author: TimeChaser
+ * @date: 2021/5/6 9:37
+ */
+public class SlowMap<K, V> extends AbstractMap<K, V> {
+
+    private List<K> keys = new ArrayList<>();
+    private List<V> values = new ArrayList<>();
+
+    @Override
+    public V get(Object key) {
+        if (!keys.contains(key)) {
+            return null;
+        }
+        return values.get(keys.indexOf(key));
+    }
+
+    @Override
+    public V put(K key, V value) {
+        V oldValue = get(key);
+        if (!keys.contains(key)) {
+            keys.add(key);
+            values.add(value);
+        } else {
+            values.set(keys.indexOf(key), value);
+        }
+        return oldValue;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        Set<Entry<K, V>> set = new HashSet<>();
+        Iterator<K> kIterator = keys.iterator();
+        Iterator<V> vIterator = values.iterator();
+        while (kIterator.hasNext()) {
+            set.add(new MapEntry<>(kIterator.next(), vIterator.next()));
+        }
+        return set;
+    }
+
+    public static void main(String[] args) {
+        SlowMap<String, String> stringStringSlowMap = new SlowMap<>();
+        stringStringSlowMap.put("CAMEROON", "Yaounde");
+        stringStringSlowMap.put("CONGO", "Brazzaville");
+        stringStringSlowMap.put("ANGOLA", "Luanda");
+        System.out.println(stringStringSlowMap);
+        System.out.println(stringStringSlowMap.get("CONGO"));
+        System.out.println(stringStringSlowMap.entrySet());
+    }
+}
