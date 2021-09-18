6e52605c94eba850dcea1e3effb66e2a92960964
TimeChaser <1872101334@qq.com>
2021-09-18 14:03:35 +0800

    二叉树遍历

---
+++ src/top/irvingsoft/chengyun/binarytree/Node.java
@@ -0,0 +1,25 @@
+package top.irvingsoft.chengyun.binarytree;
+
+/**
+ * 二叉树节点
+ *
+ * @author TimeChaser
+ * @date 2021/9/18 10:07
+ */
+public class Node {
+
+    public int value;
+    public Node left;
+    public Node right;
+
+    public Node(int data) {
+        this.value = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Node{" +
+                "value=" + value +
+                '}';
+    }
+}
---
+++ src/top/irvingsoft/chengyun/binarytree/Traverse.java
@@ -0,0 +1,123 @@
+package top.irvingsoft.chengyun.binarytree;
+
+import java.util.Stack;
+
+/**
+ * Page 106
+ *
+ * @author TimeChaser
+ * @date 2021/9/18 10:06
+ */
+public class Traverse {
+
+    public static void preOrderRecur(Node head) {
+
+        if (head == null) {
+            return;
+        }
+        System.out.print(head.value + " ");
+        preOrderRecur(head.left);
+        preOrderRecur(head.right);
+    }
+
+    public static void inOrderRecur(Node head) {
+
+        if (head == null) {
+            return;
+        }
+        inOrderRecur(head.left);
+        System.out.print(head.value + " ");
+        inOrderRecur(head.right);
+    }
+
+    public static void posOrderRecur(Node head) {
+
+        if (head == null) {
+            return;
+        }
+        posOrderRecur(head.left);
+        posOrderRecur(head.right);
+        System.out.print(head.value + " ");
+    }
+
+    public static void preOrder(Node head) {
+
+        System.out.println("Pre-Order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.add(head);
+            while (!stack.isEmpty()) {
+                head = stack.pop();
+                System.out.print(head.value + " ");
+                if (head.right != null) {
+                    stack.push(head.right);
+                }
+                if (head.left != null) {
+                    stack.push(head.left);
+                }
+            }
+        }
+    }
+
+    public static void inOrder(Node head) {
+
+        System.out.println("In-Order: ");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            while (!stack.isEmpty() || head != null) {
+                if (head != null) {
+                    stack.push(head);
+                    head = head.left;
+                } else {
+                    head = stack.pop();
+                    System.out.print(head.value + " ");
+                    head = head.right;
+                }
+            }
+        }
+    }
+
+    public static void posOrder1(Node head) {
+
+        System.out.println("Pos-Order: ");
+        if (head != null) {
+            Stack<Node> s1 = new Stack<>();
+            Stack<Node> s2 = new Stack<>();
+            s1.push(head);
+            while (!s1.isEmpty()) {
+                head = s1.pop();
+                s2.push(head);
+                if (head.left != null) {
+                    s1.push(head.left);
+                }
+                if (head.right != null) {
+                    s1.push(head.right);
+                }
+            }
+            while (!s2.isEmpty()) {
+                System.out.print(s2.pop().value + " ");
+            }
+        }
+    }
+
+    public static void posOrder2(Node head) {
+
+        System.out.println("Pos-Order");
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+            stack.push(head);
+            Node cur = null;
+            while (!stack.isEmpty()) {
+                cur = stack.peek();
+                if (cur.left != null && head != cur.left && head != cur.right) {
+                    stack.push(cur.left);
+                } else if (cur.right != null && head != cur.right) {
+                    stack.push(cur.right);
+                } else {
+                    System.out.println(stack.pop().value + " ");
+                    head = cur;
+                }
+            }
+        }
+    }
+}
