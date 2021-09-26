c784da885cca42b7bf680cb2e219019da808a9e9
TimeChaser <1872101334@qq.com>
2021-09-26 19:46:40 +0800

    兑换硬币的方法数、整数移动

---
+++ src/top/irvingsoft/chengyun/recursionandynamic/Coins.java
@@ -0,0 +1,96 @@
+package top.irvingsoft.chengyun.recursionandynamic;
+
+/**
+ * Page 214
+ *
+ * @author TimeChaser
+ * @date 2021/9/25 21:56
+ */
+public class Coins {
+
+    public static int coins1(int[] arr, int aim) {
+
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        return process1(arr, 0, aim);
+    }
+
+    public static int process1(int[] arr, int index, int aim) {
+
+        int res = 0;
+        if (index == arr.length) {
+            res = aim == 0 ? 1 : 0;
+        } else {
+            // 每个金额从零个开始计算
+            for (int i = 0; arr[index] * i <= aim; i++) {
+                res += process1(arr, index + 1, aim - arr[index] * i);
+            }
+        }
+        return res;
+    }
+
+    public static int coins2(int[] arr, int aim) {
+
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        int[][] map = new int[arr.length + 1][aim + 1];
+        return process2(arr, 0, aim, map);
+    }
+
+    public static int process2(int[] arr, int index, int aim, int[][] map) {
+
+        int res = 0;
+        if (index == arr.length) {
+            res = aim == 0 ? 1 : 0;
+        } else {
+            int mapValue = 0;
+            for (int i = 0; arr[index] * i <= aim; i++) {
+                // 先查缓存表，缓存表不存在则走递归
+                mapValue = map[index + 1][aim - arr[index] * i];
+                if (mapValue != 0) {
+                    res += mapValue == -1 ? 0 : mapValue;
+                } else {
+                    res += process2(arr, index + 1, aim - arr[index] * i, map);
+                }
+            }
+        }
+        // 将第 index 位上目标为 aim 的方法数进行缓存
+        map[index][aim] = res == 0 ? -1 : res;
+        return res;
+    }
+
+    public static int coins3(int[] arr, int aim) {
+
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+
+        int[][] dp = new int[arr.length][aim + 1];
+        // 组成 0 元的方法有 1 种
+        for (int i = 0; i < arr.length; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 1; arr[0] * j <= aim; j++) {
+            dp[0][arr[0] * j] = 1;
+        }
+        int num;
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 1; j <= aim; j++) {
+                num = 0;
+                for (int k = 0; j - arr[i] * k >= 0; k++) {
+                    num += dp[i - 1][j - arr[i] * k];
+                }
+                dp[i][j] = num;
+            }
+        }
+        return dp[arr.length - 1][aim];
+    }
+
+    public static void main(String[] args) {
+
+        int[] ints = {5, 10, 25, 1};
+        System.out.println(coins1(ints, 100));
+    }
+}
--- src/top/irvingsoft/exam/integerMove/IntegerMove.java
+++ src/top/irvingsoft/exam/integerMove/IntegerMove.java
@@ -1,40 +1,45 @@
 package top.irvingsoft.exam.integerMove;
 
-public class IntegerMove {
-
-    private static final Integer[] ints = new Integer[]{6, 4, -3, 5, -2, -1, 0, 1, -9};
-
-    public static void main(String[] args) {
-
-        Integer[] integers = new Integer[200];
-        int index = 0;
-
-        for (Integer anInt : ints) {
-            if (anInt > 0) {
-                integers[index++] = anInt;
-            }
-        }
-
-        for (Integer anInt : ints) {
-            if (anInt == 0) {
-                integers[index++] = anInt;
-            }
-        }
-
-        for (Integer anInt : ints) {
-            if (anInt < 0) {
-                integers[index++] = anInt;
-            }
-        }
-
-        for (int i = 0; i < index - 1; i++) {
-            System.out.printf("%s ", integers[i]);
-        }
-        System.out.println(integers[index - 1]);
-    }
-}
+import java.util.Arrays;
 
 /**
  * 正数左移，负数右移
+ * <p>
  * 要求时间复杂度 O(n)，空间复杂度 O(1)
+ * <p>
+ * 本解法使用双指针：一个从前往后搜索负数，一个从后往前搜索正数。然后判断是否走完全程，否，则交换
+ *
+ * @author TimeChaser
+ * @date 2021/9/26 19:36
  */
+public class IntegerMove {
+
+    public static void move(int[] arr) {
+
+        int begin = 0, end = arr.length - 1;
+        while (true) {
+            while (begin < arr.length && arr[begin] >= 0) {
+                begin++;
+            }
+            while (end >= 0 && arr[end] < 0) {
+                end--;
+            }
+            if (begin > end) {
+                break;
+            }
+            swap(arr, begin, end);
+        }
+    }
+
+    public static void swap(int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] ints = {6, -10, 4, -3, 5, -2, -1, 0, 1, -9};
+        move(ints);
+        System.out.println(Arrays.toString(ints));
+    }
+}
