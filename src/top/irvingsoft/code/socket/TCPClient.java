267627f5ae86084c23045e7d7bc0d5c4c800bfa2
TimeChaser <1872101334@qq.com>
2021-10-11 22:45:15 +0800

    TCP Server and Client!

---
+++ src/top/irvingsoft/code/socket/TCPClient.java
@@ -0,0 +1,86 @@
+package top.irvingsoft.code.socket;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @author TimeChaser
+ * @date 2021/10/11 21:01
+ */
+public class TCPClient {
+
+    public static String EXIT = "exit";
+
+    public static boolean validatePort(String s) {
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (!Character.isDigit(aChar)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        if (validatePort(args[1])) {
+            Socket client = null;
+            Thread inputThread = null;
+            Thread outputThread = null;
+            try {
+                System.out.println("连接到主机：" + args[0] + "，端口号：" + args[1]);
+                client = new Socket(args[0], Integer.parseInt(args[1]));
+                System.out.println("远程主机地址：" + client.getRemoteSocketAddress());
+                InputStream inputFromServer = client.getInputStream();
+                DataInputStream in = new DataInputStream(inputFromServer);
+                OutputStream outputToServer = client.getOutputStream();
+                DataOutputStream out = new DataOutputStream(outputToServer);
+                Scanner scan = new Scanner(System.in);
+
+                inputThread = new Thread(() -> {
+                    try {
+                        while (true) {
+                            String inputString = in.readUTF();
+                            if (inputString.length() != 0) {
+                                System.out.println("Client: " + inputString);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                outputThread = new Thread(() -> {
+                    try {
+                        while (true) {
+                            String outputString = scan.nextLine();
+                            if (outputString.equals(EXIT)) {
+                                break;
+                            }
+                            if (outputString.length() != 0) {
+                                out.writeUTF(outputString);
+                            }
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                });
+                inputThread.start();
+                outputThread.start();
+
+                outputThread.join();
+                inputThread.interrupt();
+            } catch (IOException | InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (client != null) {
+                        client.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
---
+++ src/top/irvingsoft/code/socket/TCPServer.java
@@ -0,0 +1,102 @@
+package top.irvingsoft.code.socket;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/**
+ * @author TimeChaser
+ * @date 2021/10/11 21:09
+ */
+public class TCPServer extends Thread {
+
+    public static final String EXIT = "exit";
+    private static ServerSocket serverSocket;
+
+    public TCPServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+        serverSocket.setSoTimeout(10000000);
+    }
+
+    public static boolean validatePort(String s) {
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (!Character.isDigit(aChar)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (validatePort(args[0])) {
+            new TCPServer(Integer.parseInt(args[0])).start();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        Socket socket = null;
+        Thread inputThread = null;
+        Thread outputThread = null;
+        try {
+            System.out.println("等待客户端的连接，端口号为：" + serverSocket.getLocalPort());
+            socket = serverSocket.accept();
+            System.out.println("远程客户端主机地址为：" + socket.getRemoteSocketAddress());
+            InputStream inputFromClient = socket.getInputStream();
+            DataInputStream in = new DataInputStream(inputFromClient);
+            OutputStream outToClient = socket.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToClient);
+            Scanner scan = new Scanner(System.in);
+
+            inputThread = new Thread(() -> {
+                try {
+                    while (true) {
+
+                        String inputString = in.readUTF();
+                        if (inputString.length() != 0) {
+                            System.out.println("Client: " + inputString);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            outputThread = new Thread(() -> {
+                try {
+                    while (true) {
+                        String outputString = scan.nextLine();
+                        if (outputString.equals(EXIT)) {
+                            break;
+                        }
+                        if (outputString.length() != 0) {
+                            out.writeUTF(outputString);
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            inputThread.start();
+            outputThread.start();
+
+            outputThread.join();
+            inputThread.interrupt();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null) {
+                    socket.close();
+                }
+                if (serverSocket != null) {
+                    serverSocket.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
