b390af015b65dfd735616bd7b46db571393bc5cf
TimeChaser <1872101334@qq.com>
2021-11-01 17:26:49 +0800

    整数转罗马数字

---
+++ src/top/irvingsoft/leetcode/code12/Solution.java
@@ -0,0 +1,37 @@
+package top.irvingsoft.leetcode.code12;
+
+/**
+ * 整数转罗马数字
+ *
+ * @author TimeChaser
+ * @since 2021/11/1 17:06
+ */
+public class Solution {
+
+    private static final int[] VALUES = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    private static final String[] SYMBOLS = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < VALUES.length; i++) {
+            int value = VALUES[i];
+            String symbol = SYMBOLS[i];
+            while (num >= value) {
+                num -= value;
+                sb.append(symbol);
+            }
+            if (num == 0) {
+                break;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(3));
+        System.out.println(intToRoman(4));
+        System.out.println(intToRoman(9));
+        System.out.println(intToRoman(58));
+        System.out.println(intToRoman(1994));
+    }
+}
