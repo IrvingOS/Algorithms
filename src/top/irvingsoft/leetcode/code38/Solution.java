6ff64461602e208f8dbd7a1c13e22695e88b3dc0
TimeChaser <1872101334@qq.com>
2021-11-02 10:18:07 +0800

    外观数列

---
+++ src/top/irvingsoft/leetcode/code38/Solution.java
@@ -0,0 +1,35 @@
+package top.irvingsoft.leetcode.code38;
+
+/**
+ * 外观数列
+ *
+ * @author TimeChaser
+ * @since 2021/11/2 9:58
+ */
+public class Solution {
+
+    public static String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        String s = countAndSay(n - 1);
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        char ch = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != ch) {
+                sb.append(count).append(ch);
+                count = 1;
+                ch = s.charAt(i);
+            } else {
+                count++;
+            }
+        }
+        sb.append(count).append(ch);
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countAndSay(6));
+    }
+}
