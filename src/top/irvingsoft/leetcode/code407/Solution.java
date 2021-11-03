d703ea00eb07a562ab9660d211b439e613f352f5
TimeChaser <1872101334@qq.com>
2021-11-03 10:48:55 +0800

    接雨水 ⅱ

---
+++ src/top/irvingsoft/leetcode/code407/Solution.java
@@ -0,0 +1,118 @@
+package top.irvingsoft.leetcode.code407;
+
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 接雨水 ⅱ
+ *
+ * @author TimeChaser
+ * @since 2021/11/3 8:49
+ */
+public class Solution {
+
+    /**
+     * 最小堆
+     * <p>
+     * 先将矩形的四周入优先级队列，然后从外向中心靠拢
+     * <p>
+     * 最小堆优先级队列保证了容量的有效性
+     */
+    public static int trapRainWaterMinimumHeap(int[][] heightMap) {
+        if (heightMap.length <= 2 || heightMap[0].length <= 2) {
+            return 0;
+        }
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        boolean[][] visit = new boolean[m][n];
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.offer(new int[]{i * n + j, heightMap[i][j]});
+                    visit[i][j] = true;
+                }
+            }
+        }
+        int result = 0;
+        // 方向指示，从当前优先级队列的队头节点向四周扩散
+        int[] dirs = {-1, 0, 1, 0, -1};
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            for (int k = 0; k < 4; k++) {
+                int nx = curr[0] / n + dirs[k];
+                int ny = curr[0] % n + dirs[k + 1];
+                if (nx >= 0 && nx < m && ny >= 0 && ny < n && !visit[nx][ny]) {
+                    // 队头节点比当前节点高，获得雨水容量
+                    if (curr[1] > heightMap[nx][ny]) {
+                        result += curr[1] - heightMap[nx][ny];
+                    }
+                    // 当前节点入队列，高度取原节点和当前节点的最高值
+                    pq.offer(new int[]{nx * n + ny, Math.max(heightMap[nx][ny], curr[1])});
+                    visit[nx][ny] = true;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static int trapRainWaterBFS(int[][] heightMap) {
+        int m = heightMap.length;
+        int n = heightMap[0].length;
+        int[] dirs = {-1, 0, 1, 0, -1};
+        int maxHeight = 0;
+
+        for (int[] ints : heightMap) {
+            for (int anInt : ints) {
+                maxHeight = Math.max(maxHeight, anInt);
+            }
+        }
+        int[][] water = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                water[i][j] = maxHeight;
+            }
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    if (water[i][j] > heightMap[i][j]) {
+                        water[i][j] = heightMap[i][j];
+                        queue.offer(new int[]{i, j});
+                    }
+                }
+            }
+        }
+        while (!queue.isEmpty()) {
+            int[] curr = queue.poll();
+            int x = curr[0];
+            int y = curr[1];
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dirs[i];
+                int ny = y + dirs[i + 1];
+                if (nx < 0 || nx >= m || ny < 0 || ny >= n) {
+                    continue;
+                }
+                if (water[x][y] < water[nx][ny] && water[nx][ny] > heightMap[nx][ny]) {
+                    water[nx][ny] = Math.max(water[x][y], heightMap[nx][ny]);
+                    queue.offer(new int[]{nx, ny});
+                }
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res += water[i][j] - heightMap[i][j];
+            }
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] ints = {{3, 3, 3, 3, 3}, {3, 2, 2, 2, 3}, {3, 2, 1, 2, 3}, {3, 2, 2, 2, 3}, {3, 3, 3, 3, 3}};
+        System.out.println(trapRainWaterMinimumHeap(ints));
+    }
+}
