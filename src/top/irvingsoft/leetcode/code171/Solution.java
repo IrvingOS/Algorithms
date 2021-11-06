768dacc9b55b96d3c6a1a9dcb398c7b6c78ec581
TimeChaser <1872101334@qq.com>
2021-11-06 11:45:46 +0800

    Excel 表列序号

---
+++ src/top/irvingsoft/leetcode/code171/Solution.java
@@ -0,0 +1,24 @@
+package top.irvingsoft.leetcode.code171;
+
+/**
+ * Excel 表列序号
+ *
+ * @author TimeChaser
+ * @since 2021/11/6 11:41
+ */
+public class Solution {
+
+    public static int titleToNumber(String columnTitle) {
+        int result = 0;
+        for (int i = 0; i < columnTitle.length(); i++) {
+            result = result * 26 + columnTitle.charAt(i) - 'A' + 1;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(titleToNumber("A"));
+        System.out.println(titleToNumber("AB"));
+        System.out.println(titleToNumber("ZY"));
+    }
+}
