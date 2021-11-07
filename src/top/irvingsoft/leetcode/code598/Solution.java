62d7bf93b05a06443f4b8f3f431449fd933508ff
TimeChaser <1872101334@qq.com>
2021-11-07 19:26:56 +0800

    范围求和 ⅱ

---
+++ src/top/irvingsoft/leetcode/code598/Solution.java
@@ -0,0 +1,24 @@
+package top.irvingsoft.leetcode.code598;
+
+/**
+ * 范围求和 ⅱ
+ *
+ * @author TimeChaser
+ * @since 2021/11/7 19:20
+ */
+public class Solution {
+
+    public static int maxCount(int m, int n, int[][] ops) {
+        int minI = m;
+        int minJ = n;
+        for (int[] op : ops) {
+            minI = Math.min(minI, op[0]);
+            minJ = Math.min(minJ, op[1]);
+        }
+        return minI * minJ;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
