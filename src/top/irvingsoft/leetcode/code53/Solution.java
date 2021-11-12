67f25c9c997b25be3f2b9863892aba64fea10b9d
TimeChaser <1872101334@qq.com>
2021-11-12 16:18:43 +0800

    最大子序和

---
+++ src/top/irvingsoft/leetcode/code53/Solution.java
@@ -0,0 +1,24 @@
+package top.irvingsoft.leetcode.code53;
+
+/**
+ * 最大子序和
+ *
+ * @author TimeChaser
+ * @since 2021/11/12 15:22
+ */
+public class Solution {
+
+    public static int maxSubArray(int[] nums) {
+        int pre = 0;
+        int result = nums[0];
+        for (int num : nums) {
+            pre = Math.max(pre + num, num);
+            result = Math.max(result, pre);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+}
