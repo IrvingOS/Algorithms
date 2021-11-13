9e826e3f556e935eebac13abf57b6226d9c97f9a
TimeChaser <1872101334@qq.com>
2021-11-13 10:49:16 +0800

    买卖股票的最佳时机

---
+++ src/top/irvingsoft/leetcode/code121/Solution.java
@@ -0,0 +1,28 @@
+package top.irvingsoft.leetcode.code121;
+
+/**
+ * 买卖股票的最佳时机
+ *
+ * @author TimeChaser
+ * @since 2021/11/13 10:22
+ */
+public class Solution {
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+        System.out.println(maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+}
