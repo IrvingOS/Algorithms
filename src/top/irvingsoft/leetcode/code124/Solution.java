2624deac5e4374c58c2e326dd038449335bfaa91
TimeChaser <1872101334@qq.com>
2021-11-14 15:56:42 +0800

    二叉树的最大路径和

--- src/top/irvingsoft/leetcode/code124/Solution.java
+++ src/top/irvingsoft/leetcode/code124/Solution.java
@@ -7,4 +7,22 @@ package top.irvingsoft.leetcode.code124;
  * @since 2021/11/14 10:31
  */
 public class Solution {
+
+    private static int maxSum = Integer.MIN_VALUE;
+
+    public static int maxPathSum(TreeNode root) {
+        process(root);
+        return maxSum;
+    }
+
+    public static int process(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int mid = root.val;
+        int left = Math.max(process(root.left), 0);
+        int right = Math.min(process(root.right), 0);
+        maxSum = Math.max(maxSum, left + mid + right);
+        return mid + Math.max(left, right);
+    }
 }
