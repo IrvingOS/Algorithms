1e8de9ac6a23d9b983a4e10ce9b05ce91445b58d
TimeChaser <1872101334@qq.com>
2021-11-14 20:24:01 +0800

    排序链表

--- src/top/irvingsoft/leetcode/code148/Solution.java
+++ src/top/irvingsoft/leetcode/code148/Solution.java
@@ -8,8 +8,47 @@ package top.irvingsoft.leetcode.code148;
  */
 public class Solution {
 
-    public static ListNode sortList(ListNode head) {
+    public ListNode sortList(ListNode head) {
+        return mergeSort(head);
+    }
 
-        return null;
+    private ListNode mergeSort(ListNode head) {
+        if(head == null || head.next == null) {
+            return head;
+        }
+        ListNode mid = getMid(head);
+        ListNode cur = head;
+        while(cur.next != mid) {
+            cur = cur.next;
+        }
+        cur.next = null;
+        return merge(mergeSort(head), mergeSort(mid));
+    }
+
+    private ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode left, ListNode right) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while(left != null && right != null) {
+            if(left.val < right.val) {
+                cur.next = left;
+                left = left.next;
+            } else {
+                cur.next = right;
+                right = right.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = left != null ? left : right;
+        return dummy.next;
     }
 }
