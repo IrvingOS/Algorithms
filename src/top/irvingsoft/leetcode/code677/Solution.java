3cb0b7923106e26300f5e814007067a75f3774ec
TimeChaser <1872101334@qq.com>
2021-11-14 10:30:29 +0800

    买卖股票的最佳时机 ⅱ

--- src/top/irvingsoft/leetcode/code677/Solution.java
+++ src/top/irvingsoft/leetcode/code677/Solution.java
@@ -35,3 +35,67 @@ class MapSum {
         return sum;
     }
 }
+
+class MapSumHash {
+
+    private final Map<String, Integer> map;
+    private final Map<String, Integer> prefixMap;
+
+    public MapSumHash() {
+        this.map = new HashMap<>();
+        this.prefixMap = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        for (int i = 1; i <= key.length(); i++) {
+            String prefix = key.substring(0, i);
+            prefixMap.put(prefix, prefixMap.getOrDefault(prefix, 0) + delta);
+        }
+    }
+
+    public int sum(String prefix) {
+        return prefixMap.getOrDefault(prefix, 0);
+    }
+}
+
+class MapSumTrie {
+
+    private final TrieNode root;
+    private final Map<String, Integer> map;
+
+    public MapSumTrie() {
+        this.root = new TrieNode();
+        this.map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        TrieNode node = root;
+        for (char ch : key.toCharArray()) {
+            if (node.next[ch - 'a'] == null) {
+                node.next[ch - 'a'] = new TrieNode();
+            }
+            node = node.next[ch - 'a'];
+            node.val += delta;
+        }
+    }
+
+    public int sum(String prefix) {
+        TrieNode node = root;
+        for (char ch : prefix.toCharArray()) {
+            if (node.next[ch - 'a'] == null) {
+                return 0;
+            }
+            node = node.next[ch - 'a'];
+        }
+        return node.val;
+    }
+
+    public class TrieNode {
+        int val = 0;
+        TrieNode[] next = new TrieNode[26];
+    }
+}
