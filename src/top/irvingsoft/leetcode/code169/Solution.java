ce75384eeb24f20555290d59223232ecd52952db
TimeChaser <1872101334@qq.com>
2021-11-15 09:57:37 +0800

    多数元素

---
+++ src/top/irvingsoft/leetcode/code169/Solution.java
@@ -0,0 +1,22 @@
+package top.irvingsoft.leetcode.code169;
+
+/**
+ * 多数元素
+ *
+ * @author TimeChaser
+ * @since 2021/11/15 9:57
+ */
+public class Solution {
+
+    public int majorityElement(int[] nums) {
+        int hero = 0;
+        int blood = 0;
+        for(int num : nums) {
+            if(blood == 0) {
+                hero = num;
+            }
+            blood += hero == num ? 1 : -1;
+        }
+        return hero;
+    }
+}
