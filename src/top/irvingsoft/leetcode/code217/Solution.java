d4bdbf6f335a224d52bf7c33c78d837d9ee574e2
TimeChaser <1872101334@qq.com>
2021-11-15 10:17:35 +0800

    存在重复元素

---
+++ src/top/irvingsoft/leetcode/code217/Solution.java
@@ -0,0 +1,38 @@
+package top.irvingsoft.leetcode.code217;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 存在重复元素
+ *
+ * @author TimeChaser
+ * @since 2021/11/15 10:14
+ */
+public class Solution {
+
+    public boolean containsDuplicateHash(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containsDuplicateSelectionSort(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            int temp = nums[i];
+            int j = i;
+            while (j > 0 && nums[j - 1] > temp) {
+                j--;
+            }
+            if (j > 0 && nums[j - 1] == temp) {
+                return true;
+            }
+            nums[j] = temp;
+        }
+        return false;
+    }
+}
