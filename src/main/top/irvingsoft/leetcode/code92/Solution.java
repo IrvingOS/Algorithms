43a6709145ee74653318f4f3529e42c5d6f0c073
TimeChaser <1872101334@qq.com>
2021-11-19 14:46:13 +0800

    不同的二叉查找树 ⅱ

---
+++ src/main/top/irvingsoft/leetcode/code92/Solution.java
@@ -0,0 +1,41 @@
+package top.irvingsoft.leetcode.code92;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 不同的二叉查找树 ⅱ
+ *
+ * @author TimeChaser
+ * @since 2021/11/19 10:57
+ */
+public class Solution {
+
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new ArrayList<>();
+        }
+        return dfs(1, n);
+    }
+
+    private List<TreeNode> dfs(int left, int right) {
+        List<TreeNode> result = new ArrayList<>();
+        if (left > right) {
+            result.add(null);
+            return result;
+        }
+        for (int i = left; i <= right; i++) {
+            List<TreeNode> leftNodes = dfs(left, i - 1);
+            List<TreeNode> rightNodes = dfs(i + 1, right);
+            for (TreeNode leftNode : leftNodes) {
+                for (TreeNode rightNode : rightNodes) {
+                    TreeNode curNode = new TreeNode(i);
+                    curNode.left = leftNode;
+                    curNode.right = rightNode;
+                    result.add(curNode);
+                }
+            }
+        }
+        return result;
+    }
+}
