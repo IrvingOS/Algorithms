ba8850f1de71b06acb043d2334bde38cdc48629a
TimeChaser <1872101334@qq.com>
2021-11-23 17:20:15 +0800

    找到字符串中所有字母异位词

---
+++ src/main/top/irvingsoft/leetcode/code438/Solution.java
@@ -0,0 +1,42 @@
+package top.irvingsoft.leetcode.code438;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 找到字符串中所有字母异位词
+ *
+ * @author TimeChaser
+ * @since 2021/11/23 17:14
+ */
+public class Solution {
+
+    public List<Integer> findAnagrams(String s, String p) {
+        int[] sCount = new int[26];
+        int[] pCount = new int[26];
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+        }
+        List<Integer> result = new ArrayList<>();
+        int left = 0;
+        int right = 0;
+        while (right < s.length()) {
+            int countIndexRight = s.charAt(right) - 'a';
+            sCount[countIndexRight]++;
+            while (sCount[countIndexRight] > pCount[countIndexRight]) {
+                sCount[s.charAt(left) - 'a']--;
+                left++;
+            }
+            if (right - left + 1 == p.length()) {
+                if (Arrays.equals(pCount, sCount)) {
+                    result.add(left);
+                }
+                sCount[s.charAt(left) - 'a']--;
+                left++;
+            }
+            right++;
+        }
+        return result;
+    }
+}
