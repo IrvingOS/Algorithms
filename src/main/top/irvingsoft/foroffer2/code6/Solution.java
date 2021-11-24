213e989a86f3420fc090a74f29da0aac0f8c84c0
TimeChaser <1872101334@qq.com>
2021-11-24 21:41:43 +0800

    从头到尾打印链表

---
+++ src/main/top/irvingsoft/foroffer2/code6/Solution.java
@@ -0,0 +1,27 @@
+package top.irvingsoft.foroffer2.code6;
+
+/**
+ * 从头到尾打印链表
+ *
+ * @author TimeChaser
+ * @since 2021/11/24 21:28
+ */
+public class Solution {
+
+    public int[] reversePrint(ListNode head) {
+        int count = 0;
+        ListNode cur = head;
+        while (cur != null) {
+            count++;
+            cur = cur.next;
+        }
+        int[] result = new int[count];
+        int i = count - 1;
+        cur = head;
+        while (cur != null) {
+            result[i--] = cur.val;
+            cur = cur.next;
+        }
+        return result;
+    }
+}
