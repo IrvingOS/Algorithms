464629f3d54e64d31b942aca30d5e0cd44f38dfc
TimeChaser <1872101334@qq.com>
2021-11-26 11:35:35 +0800

    二叉搜索树中的搜索

---
+++ src/main/top/irvingsoft/leetcode/code700/Solution.java
@@ -0,0 +1,24 @@
+package top.irvingsoft.leetcode.code700;
+
+/**
+ * 二叉搜索树中的搜索
+ *
+ * @author TimeChaser
+ * @since 2021/11/26 11:32
+ */
+public class Solution {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        TreeNode node = root;
+        while (node != null) {
+            if (node.val == val) {
+                return node;
+            } else if (node.val < val) {
+                node = node.right;
+            } else {
+                node = node.left;
+            }
+        }
+        return null;
+    }
+}
