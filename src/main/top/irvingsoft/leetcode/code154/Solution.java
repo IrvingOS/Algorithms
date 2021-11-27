e19ab72d537b3dfe6bde82098eed814e5855938a
TimeChaser <1872101334@qq.com>
2021-11-27 16:22:15 +0800

    旋转数组的最小数字

--- src/main/top/irvingsoft/leetcode/code154/Solution.java
+++ src/main/top/irvingsoft/leetcode/code154/Solution.java
@@ -1,22 +1,24 @@
-package top.irvingsoft.leetcode.code153;
+package top.irvingsoft.leetcode.code154;
 
 /**
- * 寻找旋转排序数组中的最小值
+ * 旋转数组的最小数字
  *
  * @author TimeChaser
- * @since 2021/11/16 18:44
+ * @since 2021/11/27 15:44
  */
 public class Solution {
 
     public int findMin(int[] nums) {
         int left = 0;
         int right = nums.length - 1;
-        while(left < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
-            if(nums[mid] < nums[right]) {
+            if (nums[mid] < nums[right]) {
                 right = mid;
-            } else {
+            } else if (nums[mid] > nums[right]) {
                 left = mid + 1;
+            } else {
+                right--;
             }
         }
         return nums[left];
