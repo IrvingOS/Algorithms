9a266d69bf7b2986b98d60fa60e9d59c88d5ac21
TimeChaser <1872101334@qq.com>
2021-11-29 12:16:20 +0800

    数值的整数次方

---
+++ src/main/top/irvingsoft/foroffer2/offer16/Solution.java
@@ -0,0 +1,32 @@
+package top.irvingsoft.foroffer2.offer16;
+
+/**
+ * 数值的整数次方
+ * <p>
+ * 快速幂
+ *
+ * @author TimeChaser
+ * @since 2021/11/29 12:12
+ */
+public class Solution {
+
+    public double myPow(double x, int n) {
+        if (x == 0) {
+            return 0;
+        }
+        long b = n;
+        double result = 1.0;
+        if (b < n) {
+            x = 1 / x;
+            b = -b;
+        }
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                result *= x;
+            }
+            x *= x;
+            b >>= 1;
+        }
+        return result;
+    }
+}
