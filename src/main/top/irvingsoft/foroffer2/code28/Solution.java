eb5fd19d5632eace2ef7cc1f51ffb7ea0ced22a5
TimeChaser <1872101334@qq.com>
2021-12-01 12:52:36 +0800

    对称的二叉树

---
+++ src/main/top/irvingsoft/foroffer2/code28/Solution.java
@@ -0,0 +1,27 @@
+package top.irvingsoft.foroffer2.code28;
+
+/**
+ * 对称的二叉树
+ *
+ * @author TimeChaser
+ * @since 2021/12/1 12:50
+ */
+public class Solution {
+
+    public boolean isSymmetric(TreeNode root) {
+        return root == null || isSame(root.left, root.right);
+    }
+
+    private boolean isSame(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left != null && right != null) {
+            if (left.val != right.val) {
+                return false;
+            }
+            return isSame(left.left, right.right) && isSame(left.right, right.left);
+        } else {
+            return false;
+        }
+    }
+}
