21bd0372097fb87e8b610e8d09d62fdc5d3ab973
TimeChaser <1872101334@qq.com>
2021-12-01 12:39:00 +0800

    连续字符

---
+++ src/main/top/irvingsoft/leetcode/code1446/Solution.java
@@ -0,0 +1,25 @@
+package top.irvingsoft.leetcode.code1446;
+
+/**
+ * 连续字符
+ *
+ * @author TimeChaser
+ * @since 2021/12/1 12:37
+ */
+public class Solution {
+
+    public int maxPower(String s) {
+        char[] chars = s.toCharArray();
+        int left = 0;
+        int right = 0;
+        int result = 0;
+        while (right < s.length()) {
+            while (left <= right && chars[right] != chars[left]) {
+                left++;
+            }
+            result = Math.max(result, right - left + 1);
+            right++;
+        }
+        return result;
+    }
+}
