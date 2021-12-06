ce5f462f5cb5129989c6e8b81b0de99bc51ec1c7
TimeChaser <1872101334@qq.com>
2021-12-06 15:38:26 +0800

    剑指 offer

---
+++ src/main/top/irvingsoft/foroffer2/code48/Solution.java
@@ -0,0 +1,28 @@
+package top.irvingsoft.foroffer2.code48;
+
+/**
+ * 最长不含重复字符的子字符串
+ *
+ * @author TimeChaser
+ * @since 2021/12/6 15:27
+ */
+public class Solution {
+
+    public int lengthOfLongestSubstring(String s) {
+        int[] count = new int[128];
+        int max = 0;
+        int left = 0;
+        int right = 0;
+        while (right < s.length()) {
+            int index = s.charAt(right);
+            count[index]++;
+            while (left <= right && count[index] != 1) {
+                count[s.charAt(left)]--;
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+            right++;
+        }
+        return max;
+    }
+}
---
+++ src/main/top/irvingsoft/foroffer2/code52/Solution.java
@@ -0,0 +1,28 @@
+package top.irvingsoft.foroffer2.code52;
+
+/**
+ * 两个链表的第一个公共结点
+ *
+ * @author TimeChaser
+ * @since 2021/12/6 15:14
+ */
+public class Solution {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode curA = headA;
+        ListNode curB = headB;
+        while (curA != curB) {
+            if (curA == null) {
+                curA = headB;
+            } else {
+                curA = curA.next;
+            }
+            if (curB == null) {
+                curB = headA;
+            } else {
+                curB = curB.next;
+            }
+        }
+        return curA;
+    }
+}
