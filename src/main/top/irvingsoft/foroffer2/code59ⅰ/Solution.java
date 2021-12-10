727d21f66e60ab2c27124f9a7d1378e371f5e976
TimeChaser <1872101334@qq.com>
2021-12-10 11:08:07 +0800

    剑指 offer

---
+++ src/main/top/irvingsoft/foroffer2/code59ⅰ/Solution.java
@@ -0,0 +1,109 @@
+package top.irvingsoft.foroffer2.code59ⅰ;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+/**
+ * 滑动窗口的最大值
+ *
+ * @author TimeChaser
+ * @since 2021/12/10 10:14
+ */
+public class Solution {
+
+    public int[] maxSlidingWindowBest(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] prefixMax = new int[n];
+        int[] suffixMax = new int[n];
+        for (int i = 0; i < n; ++i) {
+            if (i % k == 0) {
+                prefixMax[i] = nums[i];
+            } else {
+                prefixMax[i] = Math.max(prefixMax[i - 1], nums[i]);
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            if (i == n - 1 || (i + 1) % k == 0) {
+                suffixMax[i] = nums[i];
+            } else {
+                suffixMax[i] = Math.max(suffixMax[i + 1], nums[i]);
+            }
+        }
+
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i <= n - k; ++i) {
+            ans[i] = Math.max(suffixMax[i], prefixMax[i + k - 1]);
+        }
+        return ans;
+    }
+
+    public int[] maxSlidingWindowDeque(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        Deque<Integer> deque = new LinkedList<>();
+        for (int i = 0; i < k; i++) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        int[] result = new int[n - k + 1];
+        result[0] = nums[deque.peekFirst()];
+        for (int i = k; i < n; i++) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            result[i - k + 1] = nums[deque.peekFirst()];
+        }
+        return result;
+    }
+
+    public int[] maxSlidingWindowMine(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        int[] result = new int[n - k + 1];
+        int index = 0;
+        for (int i = 0; i < n - k + 1; i++) {
+            int max = nums[i];
+            for (int j = i; j < i + k; j++) {
+                max = Math.max(max, nums[j]);
+            }
+            result[index++] = max;
+        }
+        return result;
+    }
+
+    public int[] maxSlidingWindowPriorityQueue(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        Queue<int[]> queue = new PriorityQueue<>((o1, o2) -> o1[0] != o2[0] ? o2[0] - o1[0] : o2[1] - o1[1]);
+        for (int i = 0; i < k; i++) {
+            queue.offer(new int[]{nums[i], i});
+        }
+        int[] result = new int[n - k + 1];
+        result[0] = queue.peek()[0];
+        for (int i = k; i < n; i++) {
+            queue.offer(new int[]{nums[i], i});
+            while (queue.peek()[1] <= i - k) {
+                queue.poll();
+            }
+            result[i - k + 1] = queue.peek()[0];
+        }
+        return result;
+    }
+}
