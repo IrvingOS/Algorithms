6b0830e425dffe8085e2b514175ad88157280a87
TimeChaser <1872101334@qq.com>
2022-01-01 12:06:32 +0800

    消除游戏

---
+++ src/main/top/irvingsoft/leetcode/code390/Solution.java
@@ -0,0 +1,29 @@
+package top.irvingsoft.leetcode.code390;
+
+/**
+ * 消除游戏
+ *
+ * @author TimeChaser
+ * @since 2022/1/1 11:49 上午
+ */
+public class Solution {
+
+    public int lastRemaining(int n) {
+        int count = n;
+        int k = 0, step = 1;
+        int a1 = 1, an = n;
+        while(count > 1) {
+            if (k % 2 == 0) {
+                a1 += step;
+                an -= count % 2 != 0 ? step : 0;
+            } else {
+                an -= step;
+                a1 += count % 2 != 0 ? step : 0;
+            }
+            k++;
+            step <<= 1;
+            count >>= 1;
+        }
+        return a1;
+    }
+}
