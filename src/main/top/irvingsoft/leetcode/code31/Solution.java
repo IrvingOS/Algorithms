9d13a6745e9ce6f5c06901682a97f9cfa30016ca
TimeChaser <1872101334@qq.com>
2022-01-06 20:08:13 +0800

    下一个排列

---
+++ src/main/top/irvingsoft/leetcode/code31/Solution.java
@@ -0,0 +1,42 @@
+package top.irvingsoft.leetcode.code31;
+
+import java.util.Arrays;
+
+/**
+ * 下一个排列
+ *
+ * @author TimeChaser
+ * @since 2022/1/6 20:00
+ */
+public class Solution {
+
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        boolean notExist = true;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                int j = n - 1;
+                while (i < j && nums[j] <= nums[i]) {
+                    j--;
+                }
+                swap(nums, i, j);
+                j = n - 1;
+                i++;
+                while (i < j) {
+                    swap(nums, i++, j--);
+                }
+                notExist = false;
+                break;
+            }
+        }
+        if (notExist) {
+            Arrays.sort(nums);
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
