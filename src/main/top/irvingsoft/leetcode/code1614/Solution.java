3fb5a98fdc1e758a32cd1921570a8ddcd94f6656
TimeChaser <1872101334@qq.com>
2022-01-07 10:49:31 +0800

    括号的最大嵌套深度

---
+++ src/main/top/irvingsoft/leetcode/code1614/Solution.java
@@ -0,0 +1,24 @@
+package top.irvingsoft.leetcode.code1614;
+
+/**
+ * 括号的最大嵌套深度
+ *
+ * @author TimeChaser
+ * @since 2022/1/7 10:48
+ */
+public class Solution {
+
+    public int maxDepth(String s) {
+        int maxDepth = 0;
+        int size = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                size++;
+                maxDepth = Math.max(size, maxDepth);
+            } else if (c == ')') {
+                size--;
+            }
+        }
+        return maxDepth;
+    }
+}
