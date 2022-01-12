383e4b7183031dfca173a25f38c7e4898c0d6c9c
TimeChaser <1872101334@qq.com>
2022-01-12 10:24:00 +0800

    递增的三元子序列

---
+++ src/main/top/irvingsoft/leetcode/code334/Solution.java
@@ -0,0 +1,52 @@
+package top.irvingsoft.leetcode.code334;
+
+/**
+ * 递增的三元子序列
+ *
+ * @author TimeChaser
+ * @since 2022/1/12 10:18
+ */
+public class Solution {
+
+    public boolean increasingTripletGreedy(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int first = nums[0], second = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            if (num > second) {
+                return true;
+            } else if (num > first) {
+                second = num;
+            } else {
+                first = num;
+            }
+        }
+        return false;
+    }
+
+    public boolean increasingTripletPrefixSum(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int[] leftMin = new int[n];
+        leftMin[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(leftMin[i - 1], nums[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > leftMin[i] && nums[i] < rightMax[i]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
