d9f3ebb5fdb417159f0525833abe460b10f12fec
TimeChaser <1872101334@qq.com>
2022-01-13 15:21:13 +0800

    填充每个节点的下一个右侧节点指针 ⅱ

---
+++ src/main/top/irvingsoft/leetcode/code117/Solution.java
@@ -0,0 +1,35 @@
+package top.irvingsoft.leetcode.code117;
+
+/**
+ * 填充每个节点的下一个右侧节点指针 ⅱ
+ *
+ * @author TimeChaser
+ * @since 2022/1/13 14:56
+ */
+public class Solution {
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left != null) {
+            root.left.next = root.right != null ? root.right : getNext(root.next);
+        }
+        if (root.right != null) {
+            root.right.next = getNext(root.next);
+        }
+        connect(root.right);
+        connect(root.left);
+        return root;
+    }
+
+    private Node getNext(Node next) {
+        while (next != null) {
+            if (next.left != null || next.right != null) {
+                break;
+            }
+            next = next.next;
+        }
+        return next != null ? (next.left != null ? next.left : next.right) : null;
+    }
+}
