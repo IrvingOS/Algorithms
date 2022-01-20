8dc995302d7de917f1e493d4fc61f20dfe721717
TimeChaser <1872101334@qq.com>
2022-01-20 19:03:46 +0800

    石子游戏 ⅸ

---
+++ src/main/top/irvingsoft/leetcode/code2029/Solution.java
@@ -0,0 +1,21 @@
+package top.irvingsoft.leetcode.code2029;
+
+/**
+ * 石子游戏 ⅸ
+ *
+ * @author TimeChaser
+ * @since 2022/1/20 18:06
+ */
+public class Solution {
+
+    public boolean stoneGameIX(int[] stones) {
+        int[] s = new int[3];
+        for (int stone : stones) {
+            s[stone % 3]++;
+        }
+        if (s[0] % 2 == 0) {
+            return s[1] > 0 && s[2] > 0;
+        }
+        return s[1] - s[2] > 2 || s[2] - s[1] > 2;
+    }
+}
