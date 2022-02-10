d763d5d71c71575cc0881ebf313598f212466c03
TimeChaser <1872101334@qq.com>
2022-02-10 12:52:52 +0800

    二叉树的层序遍历

---
+++ src/main/top/irvingsoft/foroffer/code102/Solution.java
@@ -0,0 +1,40 @@
+package top.irvingsoft.foroffer.code102;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 二叉树的层序遍历
+ *
+ * @author TimeChaser
+ * @since 2022/2/10 12:51
+ */
+public class Solution {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<>();
+        }
+        List<List<Integer>> result = new ArrayList<>();
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> level = new ArrayList<>();
+            int currentLevelSize = queue.size();
+            for (int i = 0; i < currentLevelSize; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
