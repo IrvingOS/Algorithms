af39e8673777b91fe84b6f2a48c2bb875d8446e2
TimeChaser <1872101334@qq.com>
2022-02-23 11:10:58 +0800

    仅仅反转字母

---
+++ src/main/top/irvingsoft/leetcode/code917/Solution.java
@@ -0,0 +1,37 @@
+package top.irvingsoft.leetcode.code917;
+
+/**
+ * 仅仅反转字母
+ *
+ * @author TimeChaser
+ * @since 2022/2/23 11:08
+ */
+public class Solution {
+
+    public String reverseOnlyLetters(String s) {
+        int n = s.length();
+        char[] arr = s.toCharArray();
+        int lo = 0, hi = n - 1;
+        while (true) {
+            while (lo < hi && !Character.isLetter(s.charAt(lo))) {
+                lo++;
+            }
+            while (lo < hi && !Character.isLetter(s.charAt(hi))) {
+                hi--;
+            }
+            if (lo >= hi) {
+                break;
+            }
+            swap(arr, lo, hi);
+            lo++;
+            hi--;
+        }
+        return new String(arr);
+    }
+
+    private void swap(char[] arr, int i, int j) {
+        char temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
