182dd3dab1b180cf238ca9647ef8398e84e1e780
TimeChaser <1872101334@qq.com>
2022-03-03 10:50:12 +0800

    各位相加

---
+++ src/main/top/irvingsoft/leetcode/code258/Solution.java
@@ -0,0 +1,23 @@
+package top.irvingsoft.leetcode.code258;
+
+/**
+ * 各位相加
+ *
+ * @author TimeChaser
+ * @since 2022/3/3 10:49
+ */
+public class Solution {
+
+    public int addDigits(int num) {
+        int result = 0;
+        while (num != 0 || result >= 10) {
+            if (num == 0) {
+                num = result;
+                result = 0;
+            }
+            result += num % 10;
+            num /= 10;
+        }
+        return result;
+    }
+}
