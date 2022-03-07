91a839d3a05b745a4ec86783ec2dd11b62d00781
TimeChaser <1872101334@qq.com>
2022-03-07 22:10:37 +0800

    七进制数

---
+++ src/main/top/irvingsoft/leetcode/code504/Solution.java
@@ -0,0 +1,27 @@
+package top.irvingsoft.leetcode.code504;
+
+/**
+ * 七进制数
+ *
+ * @author TimeChaser
+ * @since 2022/3/7 10:07 PM
+ */
+public class Solution {
+
+    public String convertToBase7(int num) {
+        if (num == 0) {
+            return "0";
+        }
+        StringBuilder result = new StringBuilder();
+        boolean yes = num < 0;
+        num = Math.abs(num);
+        while (num != 0) {
+            result.append(num % 7);
+            num /= 7;
+        }
+        if (yes) {
+            result.append("-");
+        }
+        return result.reverse().toString();
+    }
+}
