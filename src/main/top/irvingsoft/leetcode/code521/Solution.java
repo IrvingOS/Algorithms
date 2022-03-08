07878781a3605fb7e573bd6464bd9492cb4e2a16
TimeChaser <1872101334@qq.com>
2022-03-08 11:57:29 +0800

    最长特殊序列 i

---
+++ src/main/top/irvingsoft/leetcode/code521/Solution.java
@@ -0,0 +1,14 @@
+package top.irvingsoft.leetcode.code521;
+
+/**
+ * 最长特殊序列 i
+ *
+ * @author TimeChaser
+ * @since 2022/3/8 11:56 AM
+ */
+public class Solution {
+
+    public int findLUSlength(String a, String b) {
+        return a.equals(b) ? -1 : Math.max(a.length(), b.length());
+    }
+}
