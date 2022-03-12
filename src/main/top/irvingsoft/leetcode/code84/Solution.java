f8e33281a7c1be5afc9427b074066e3e11b5081d
TimeChaser <1872101334@qq.com>
2022-03-12 16:39:27 +0800

    柱状图中最大的矩形

---
+++ src/main/top/irvingsoft/leetcode/code84/Solution.java
@@ -0,0 +1,35 @@
+package top.irvingsoft.leetcode.code84;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * 柱状图中最大的矩形
+ *
+ * @author TimeChaser
+ * @since 2022/3/12 3:49 PM
+ */
+public class Solution {
+
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] newHeights = new int[n + 2];
+        for (int i = 0; i < n; i++) {
+            newHeights[i + 1] = heights[i];
+        }
+        n += 2;
+        heights = newHeights;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.push(0);
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            while (heights[stack.peek()] > heights[i]) {
+                int width = i - stack.peek() - 1;
+                int height = heights[stack.pop()];
+                result = Math.max(width * height, result);
+            }
+            stack.push(i);
+        }
+        return result;
+    }
+}
