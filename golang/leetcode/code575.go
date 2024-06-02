84bafe9ebdb71a2ceb19ef86bca272d8cf2dc030
今晚打老虎 <1872101334@qq.com>
2024-06-02 22:51:09 +0800

    feat: leetcode 575

---
+++ golang/leetcode/code575.go
@@ -0,0 +1,10 @@
+package main
+
+func distributeCandies(candyType []int) int {
+	n := len(candyType)
+	count := make(map[int]bool)
+	for _, t := range candyType {
+		count[t] = true
+	}
+	return min(n/2, len(count))
+}
