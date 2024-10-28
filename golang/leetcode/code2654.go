40a69a51c493328d885a63cb7f67bba4d9687544
TimeChaser <1872101334@qq.com>
2024-10-28 08:45:38 +0800

    feat(leetcode): code 1702, 2654

---
+++ golang/leetcode/code2654.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"math"
+)
+
+func minOperations2654(nums []int) int {
+	n := len(nums)
+	count1 := 0
+	for _, num := range nums {
+		if num == 1 {
+			count1++
+		}
+	}
+	if count1 > 0 {
+		return n - count1
+	}
+	res := math.MaxInt
+	for j := n - 1; j > 0; j-- {
+		k := nums[j]
+		for i := j - 1; i >= 0; i-- {
+			k = gcd(nums[i], k)
+			if k == 1 {
+				res = min(res, n+(j-i-1))
+			}
+		}
+	}
+	if res == math.MaxInt {
+		return -1
+	}
+	return res
+}
+
+func gcd(i int, j int) int {
+	r := 0
+	for j > 0 {
+		r = i % j
+		i = j
+		j = r
+	}
+	return i
+}
